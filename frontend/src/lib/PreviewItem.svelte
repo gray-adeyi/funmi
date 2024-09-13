8d4c5db7c6e22e0ff33321d18191e18ada436f8a
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-13 14:41:46 +0100

    Implement color preview component

--- frontend/src/lib/PreviewItem.svelte
+++ frontend/src/lib/PreviewItem.svelte
@@ -1,17 +1,43 @@
 <script lang="ts">
   import "@material/web/iconbutton/icon-button.js";
   import "@material/web/icon/icon.js";
+  import { handleButtonEvents } from "@/events";
+  import { createEventDispatcher } from "svelte";
+  import { type NotificationEventPayload } from "@/types/events";
+
+  const dispatcher = createEventDispatcher<{
+    showNotification: NotificationEventPayload;
+  }>();
+
+  export let color: string | null;
+
+  const copyToClipboard = handleButtonEvents(async () => {
+    try {
+      await navigator.clipboard.writeText(color);
+      dispatcher("showNotification", {
+        message: `Copied ${color} to clipboard`,
+        action: null,
+      });
+    } catch (err) {
+      // TODO: handle err
+    }
+  });
 </script>
 
 <div class="item">
-  <span class="item__color-value">rgba(200,100,10,1)</span>
-  <md-icon-button>
+  <span class="item__color-value">{color}</span>
+  <md-icon-button
+    role="button"
+    tabindex="0"
+    on:click={copyToClipboard}
+    on:keydown={copyToClipboard}
+  >
     <md-icon>content_copy</md-icon>
   </md-icon-button>
 </div>
 
 <style>
-  .item{
+  .item {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -20,11 +46,11 @@
     border-radius: 1rem;
   }
 
-  .item__color-value{
+  .item__color-value {
     font-size: 1.5rem;
     white-space: nowrap;
     overflow: hidden;
-    width: 18rem;
+    width: 15rem;
     text-overflow: ellipsis;
   }
 </style>
