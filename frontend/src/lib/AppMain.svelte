7bdb74bdd0bfab4459eaf70fa1189a1609cdf271
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-13 14:45:44 +0100

    Implement AppMain component

--- frontend/src/lib/AppMain.svelte
+++ frontend/src/lib/AppMain.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
     import ConversionEditor from "@/lib/ConversionEditor.svelte";
     import ConversionPreview from "@/lib/ConversionPreview.svelte";
+
+    let sourceRgba = "";
+
+    function handleUpdateSourceColor(event: CustomEvent<string>) {
+        console.log("i am getting called");
+        sourceRgba = event.detail;
+    }
 </script>
 
-
 <div class="main">
-    <ConversionEditor />
-    <ConversionPreview />
+    <ConversionEditor
+        on:updateSourceColor={handleUpdateSourceColor}
+        on:showNotification
+    />
+    <ConversionPreview {sourceRgba} on:showNotification />
 </div>
 
 <style>
-    .main{
+    .main {
         display: flex;
         flex-direction: column;
         flex-grow: 1;
