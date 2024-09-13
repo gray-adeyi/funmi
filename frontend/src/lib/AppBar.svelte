44d115d2ba7027134dfc530176e30cdc1cac7762
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-13 14:42:12 +0100

    Implement menu for app bar

--- frontend/src/lib/AppBar.svelte
+++ frontend/src/lib/AppBar.svelte
@@ -1,18 +1,83 @@
 <script lang="ts">
     import FunmiLogo from "@/assets/funmi-logo.svg?component";
+    import "@material/web/iconbutton/icon-button.js";
+    import "@material/web/icon/icon.js";
+    import "@material/web/menu/menu.js";
+    import "@material/web/menu/menu-item.js";
+    import { createEventDispatcher } from "svelte";
+    import { handleButtonEvents } from "@/events";
+
+    let menuEl: HTMLElement | null = null;
+    const dispatcher = createEventDispatcher<{
+        showHelpDialog: null;
+        showAboutDialog: null;
+    }>();
+
+    const toggleMenu = handleButtonEvents(() => {
+        if (!menuEl) return;
+        (menuEl as any).open = !(menuEl as any).open;
+    });
+
+    const showHelpDialog = handleButtonEvents(() =>
+        dispatcher("showHelpDialog"),
+    );
+
+    const showAboutDialog = handleButtonEvents(() =>
+        dispatcher("showAboutDialog"),
+    );
 </script>
 
 <div class="appbar">
     <div class="appbar__brand">
         <FunmiLogo class="appbar__logo" />
     </div>
+    <span class="appbar__menu">
+        <md-icon-button
+            id="app-menu-anchor"
+            role="button"
+            tabindex="0"
+            on:click={toggleMenu}
+            on:keydown={toggleMenu}
+        >
+            <md-icon>menu</md-icon>
+        </md-icon-button>
+        <md-menu bind:this={menuEl} anchor="app-menu-anchor">
+            <md-menu-item>
+                <div
+                    slot="headline"
+                    role="button"
+                    tabindex="0"
+                    on:click={showHelpDialog}
+                    on:keydown={showHelpDialog}
+                >
+                    Help
+                </div>
+            </md-menu-item>
+            <md-menu-item>
+                <div
+                    slot="headline"
+                    role="button"
+                    tabindex="0"
+                    on:click={showAboutDialog}
+                    on:keydown={showAboutDialog}
+                >
+                    About
+                </div>
+            </md-menu-item>
+        </md-menu>
+    </span>
 </div>
 
 <style>
-    .appbar{
-        /* border: 2px solid white; */
+    .appbar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
     }
     :global(svg.appbar__logo) {
         width: 10rem;
+    }
+
+    .appbar__menu {
     }
 </style>
