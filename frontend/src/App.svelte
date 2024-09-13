24302f074c39a1c5371c0b7e3c78b3915365c8b2
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-13 14:46:20 +0100

    Implement app layout

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,21 +1,57 @@
 <script lang="ts">
-    import AppBar from '@/lib/AppBar.svelte';
-    import AppMain from '@/lib/AppMain.svelte';
+  import AppBar from "@/lib/AppBar.svelte";
+  import AppMain from "@/lib/AppMain.svelte";
+  import HelpDialog from "./lib/HelpDialog.svelte";
+  import AboutDialog from "./lib/AboutDialog.svelte";
+  import SnackBar from "./lib/SnackBar.svelte";
+  import type { NotificationEventPayload } from "./types/events";
 
+  let isHelpDialogVisible = false;
+  let isAboutDialogVisible = false;
+  let isNotificationVisible = false;
 
-  
+  let notificationPayload: NotificationEventPayload = {
+    message: "",
+    action: null,
+  };
+
+  function handleShowNotification(
+    event: CustomEvent<NotificationEventPayload>,
+  ) {
+    isNotificationVisible = true;
+    notificationPayload = event.detail;
+  }
 </script>
 
 <main class="app">
-  <AppBar />
-  <AppMain />
+  <AppBar
+    on:showHelpDialog={() => (isHelpDialogVisible = true)}
+    on:showAboutDialog={() => (isAboutDialogVisible = true)}
+  />
+  <AppMain on:showNotification={handleShowNotification} />
+
+  {#if isHelpDialogVisible}
+    <HelpDialog
+      open={isHelpDialogVisible}
+      on:close={() => (isHelpDialogVisible = false)}
+    />
+  {/if}
+  {#if isAboutDialogVisible}
+    <AboutDialog />
+  {/if}
+  {#if isNotificationVisible}
+    <SnackBar
+      {...notificationPayload}
+      on:close={() => (isNotificationVisible = false)}
+    />
+  {/if}
 </main>
 
 <style>
-.app{
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  height: 100vh;
-} 
+  .app {
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    height: 100vh;
+  }
 </style>
