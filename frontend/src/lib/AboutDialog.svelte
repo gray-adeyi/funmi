1429f80e9d47d0876d160b028d09ff1bd703321c
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-13 14:45:10 +0100

    Add blank about dialog

---
+++ frontend/src/lib/AboutDialog.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import "@material/web/dialog/dialog.js";
+    import "@material/web/button/text-button.js";
+</script>
+
+<md-dialog open>
+    <div slot="headline">He</div>
+    <div slot="content">A simple dialog with free-form content.</div>
+    <div slot="actions">
+        <md-text-button>Close</md-text-button>
+    </div>
+</md-dialog>
