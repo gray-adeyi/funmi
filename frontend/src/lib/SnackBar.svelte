3d2df07e8589862c5641f2fd8ed058eda15ec986
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-13 14:40:08 +0100

    Implement snackbar component

---
+++ frontend/src/lib/SnackBar.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+    import { handleButtonEvents } from "@/events";
+    import { createEventDispatcher, onMount } from "svelte";
+
+    const NOTIFICATION_TIMEOUT = 2 * 1000;
+
+    export let message = "Single-line snackbar with action";
+
+    export let actionText = "Action";
+
+    export let action: (() => void) | null = null;
+
+    export let position: {
+        top?: number;
+        right?: number;
+        bottom?: number;
+        left?: number;
+    } = { right: 10, bottom: 10 };
+
+    const close = handleButtonEvents(() => dispatcher("close"));
+
+    let timeoutId: number | null;
+
+    const dispatcher = createEventDispatcher<{ close: null }>();
+
+    onMount(() => {
+        timeoutId = setTimeout(() => dispatcher("close"), NOTIFICATION_TIMEOUT);
+        return () => {
+            if (timeoutId) clearTimeout(timeoutId);
+        };
+    });
+
+    $: hasAction = action !== null;
+    $: actionHandler = hasAction ? () => {} : handleButtonEvents(action);
+    $: topStyle = position?.top ? `top: ${position.top}px;` : "";
+    $: rightStyle = position?.right ? `right: ${position.right}px;` : "";
+    $: bottomStyle = position?.bottom ? `bottom: ${position.bottom}px;` : "";
+    $: leftStyle = position?.left ? `left: ${position.left}px;` : "";
+    $: positionStyle = `${topStyle}${rightStyle}${bottomStyle}${leftStyle}`;
+</script>
+
+<div class="snack-bar" style={positionStyle}>
+    <p class="snack-bar__message">{message}</p>
+    <md-text-button
+        role="button"
+        tabindex="0"
+        style:visibility={hasAction ? "" : "hidden"}
+        on:click={actionHandler}
+        on:keydown={actionHandler}>{actionText}</md-text-button
+    >
+    <md-icon-button
+        role="button"
+        tabindex="0"
+        on:click={close}
+        on:keydown={close}
+    >
+        <md-icon>close</md-icon>
+    </md-icon-button>
+</div>
+
+<style>
+    .snack-bar {
+        display: flex;
+        position: fixed;
+        justify-content: space-between;
+        gap: 1rem;
+        align-items: center;
+        padding: 0.5rem;
+        background: #121212;
+        max-width: 70vw;
+        border-radius: 8px;
+    }
+
+    .snack-bar__message {
+        color: var(--primary-bg);
+        font-size: 1.3rem;
+    }
+
+    @media screen and (prefers-color-scheme: dark) {
+        .snack-bar {
+            background: #fff;
+        }
+    }
+</style>
