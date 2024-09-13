a4a027a744102796f8938930b79c8a354d71e504
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-13 14:43:13 +0100

    Implement ConversionEditor component

--- frontend/src/lib/ConversionEditor.svelte
+++ frontend/src/lib/ConversionEditor.svelte
@@ -4,26 +4,101 @@
     import "@material/web/textfield/outlined-text-field.js";
     import "@material/web/button/filled-tonal-button.js";
     import "@material/web/icon/icon.js";
-    import colorFormats from "@/colorFormats";
+    import { CSSColorFormat } from "@/colorUtils";
+    import { handleButtonEvents } from "@/events";
+    import { createEventDispatcher } from "svelte";
+    import type { NotificationEventPayload } from "@/types/events";
+
+    let sourceFormat: CSSColorFormat = CSSColorFormat.RGBA;
+
+    let sourceColor = "rgba(200,200,100)";
+
+    const dispatcher = createEventDispatcher<{
+        showNotification: NotificationEventPayload;
+        updateSourceColor: string;
+    }>();
+
+    const supportedSourceFormats: {
+        displayName: string;
+        value: CSSColorFormat;
+    }[] = [
+        { displayName: "Hexadecimal", value: CSSColorFormat.HEXADECIMAL },
+        { displayName: "RGB/RGBA", value: CSSColorFormat.RGBA },
+        { displayName: "HSL/HSLA", value: CSSColorFormat.HSLA },
+        { displayName: "HWB", value: CSSColorFormat.HWB },
+        { displayName: "Lab", value: CSSColorFormat.LAB },
+        { displayName: "OKLab", value: CSSColorFormat.OK_LAB },
+        {
+            displayName: "Named Colors",
+            value: CSSColorFormat.COLOR_NAME_KEYWORDS,
+        },
+        { displayName: "Gray", value: CSSColorFormat.GRAY },
+        { displayName: "Color Function", value: CSSColorFormat.COLOR_FUNCTION },
+    ];
+
+    $: parsedSourceColor = sourceColor;
+
+    $: dispatcher("updateSourceColor", parsedSourceColor);
+
+    function handleSourceInput(event: InputEvent) {
+        sourceColor = (event.target as HTMLInputElement).value;
+    }
+
+    function handleSourceFormatChange(event: InputEvent) {
+        sourceFormat = (event.target as HTMLSelectElement)
+            .value as any as CSSColorFormat;
+    }
+
+    const copyFromClipboard = handleButtonEvents(async () => {
+        try {
+            const clipboardText = await navigator.clipboard.readText();
+            dispatcher("showNotification", {
+                message: `Copied ${clipboardText} from clipboard`,
+                action: null,
+            });
+            // TODO: validate pasted clipboard text
+            sourceColor = clipboardText;
+        } catch (err) {
+            // TODO: Handle error
+            throw err;
+        }
+    });
 </script>
 
 <div class="editor">
-    <md-outlined-select required>
+    <md-outlined-select
+        value={sourceFormat}
+        on:change={handleSourceFormatChange}
+        required
+    >
         <md-select-option selected aria-label="blank"
             >Select Source Format</md-select-option
         >
-        {#each colorFormats as format}
-            <md-select-option value={format.code}>
+        {#each supportedSourceFormats as format (format.value)}
+            <md-select-option value={format.value}>
                 <div slot="headline">{format.displayName}</div>
             </md-select-option>
         {/each}
     </md-outlined-select>
-    <md-outlined-text-field label="Source Color" value="rgba(200,200,100)" />
-    <md-icon-button title="paste from clipboard">
+    <md-outlined-text-field
+        label="Source Color"
+        value={sourceColor}
+        on:input={handleSourceInput}
+    />
+    <md-icon-button
+        role="button"
+        tabindex="0"
+        on:click={copyFromClipboard}
+        on:keydown={copyFromClipboard}
+        title="paste from clipboard"
+    >
         <md-icon>content_paste</md-icon>
     </md-icon-button>
     <div class="editor__preview-box-container">
-        <div class="editor__preview-box"></div>
+        <div
+            class="editor__preview-box"
+            style="background: {parsedSourceColor};"
+        ></div>
     </div>
 </div>
 
@@ -46,8 +121,6 @@
     .editor__preview-box {
         width: 100%;
         height: 100%;
-        background: red;
         border-radius: 1rem;
     }
-    
 </style>
