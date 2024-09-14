8bc048183e06542d888af25a0430fe97a478ad94
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-15 00:38:17 +0100

    Implement function to open browser

--- app.go
+++ app.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os/exec"
+	"runtime"
 )
 
 // App struct
@@ -24,4 +26,18 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) OpenBrowser(url string) error {
+	var err error
+	switch runtime.GOARCH {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	return err
 }
